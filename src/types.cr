285877f1eb6c8578cec978683d9b1053d2b7b47a
Jamie Gaskins <jgaskins@gmail.com>
2020-10-13 06:55:32 -0400

    Add booleans to value types

--- src/types.cr
+++ src/types.cr
@@ -2,6 +2,7 @@ require "uuid"
 
 module Interro
   alias Primitive = Nil |
+                    Bool |
                     String |
                     Int8 | Int16 | Int32 | Int64 |
                     Time |
